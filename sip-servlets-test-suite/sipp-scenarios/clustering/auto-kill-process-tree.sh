28feaf07a9077a65a62175eea6a666cbe4b6af18
Jean Deruelle <jean.deruelle@gmail.com>
2012-08-30 22:04:54 +0200

    reverting autokill

--- sip-servlets-test-suite/sipp-scenarios/clustering/auto-kill-process-tree.sh
+++ sip-servlets-test-suite/sipp-scenarios/clustering/auto-kill-process-tree.sh
@@ -7,16 +7,15 @@ echo "Trying Killing script $2 child process with parent pid = $pid"
 for child in $(ps -o pid,ppid ax | \
    awk "{ if ( \$2 == $pid ) { print \$1 }}")
 do
-  echo "Soft Killing script $2 child process $child because parent pid = $pid"
+  echo "Killing script $2 child process $child because parent pid = $pid"
   kill $child # clean kill with shutdown instruction
-  sleep 1 # give some time for the clean shutdown to reach out
+  sleep .4 # give some time for the clean shutdown to reach out
   echo "Hard Killing script $2 child process $child because parent pid = $pid"
   kill -9 $child # kill it immediately without wasting more time
-  killed="yes"
+  export killed="yes"
 done
 
-if [ "no" = "$killed" ]; then
+if [ "no" = $killed ]; then
   echo "The app server is not dead? We will sleep. We must raise error here, because this server should have been dead."
-  sleep 10
+  sleep 1
 fi
-
