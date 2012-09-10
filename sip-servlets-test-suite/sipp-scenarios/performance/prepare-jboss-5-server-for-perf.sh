79419af004de245fefb27b434d40975cbcdffaf1
Jean Deruelle <jean.deruelle@gmail.com>
2012-09-10 17:19:43 +0200

    removing the offline flag for compiling the examples for perf

--- sip-servlets-test-suite/sipp-scenarios/performance/prepare-jboss-5-server-for-perf.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/prepare-jboss-5-server-for-perf.sh
@@ -12,13 +12,13 @@ if [ $# -ne 0 ]; then
 	case $1 in	
 	    proxy)
 	    		echo "Distributed example used is proxy"
-	    		mvn clean install -o -f ../../../sip-servlets-examples/location-service/pom.xml
+	    		mvn clean install -f ../../../sip-servlets-examples/location-service/pom.xml
 				cp ../../../sip-servlets-examples/location-service/target/location-service-*.war $JBOSS_HOME/server/default/deploy
 				cp ../../../sip-servlets-examples/location-service/locationservice-dar.properties $JBOSS_HOME/server/default/conf/dars/mobicents-dar.properties
 	            ;;
 	    b2bua)
 	            echo "Distributed example used is b2bua"
-	    		mvn clean install -o -f ../../../sip-servlets-examples/call-forwarding/pom.xml
+	    		mvn clean install -f ../../../sip-servlets-examples/call-forwarding/pom.xml
 				cp ../../../sip-servlets-examples/call-forwarding/target/call-forwarding-*.war $JBOSS_HOME/server/default/deploy
 				cp ../../../sip-servlets-examples/call-forwarding/call-forwarding-b2bua-servlet-dar.properties $JBOSS_HOME/server/default/conf/dars/mobicents-dar.properties
 	            ;;	    	   
